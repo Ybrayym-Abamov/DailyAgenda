e01767b615c8637c8ff36d4c849177db813ec26d
Ybrayym <ybrayymjan90@gmail.com>
2020-08-19 12:09:06 -0400

    Updates

---
+++ precache-manifest.4531c86269500defdf812e62fb5154c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cba65d617d9442f62cb93a7a455befe",
+    "url": "/DailyAgenda/index.html"
+  },
+  {
+    "revision": "96c145e8b2002c5ee080",
+    "url": "/DailyAgenda/static/css/main.649c2adf.chunk.css"
+  },
+  {
+    "revision": "aebd39fc229f51e4a49c",
+    "url": "/DailyAgenda/static/js/2.9aa71a27.chunk.js"
+  },
+  {
+    "revision": "6d6c0d1371e570614ef23e12cbfed171",
+    "url": "/DailyAgenda/static/js/2.9aa71a27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96c145e8b2002c5ee080",
+    "url": "/DailyAgenda/static/js/main.a5c71d91.chunk.js"
+  },
+  {
+    "revision": "976db251a4e1a92b9585",
+    "url": "/DailyAgenda/static/js/runtime-main.44c26004.js"
+  }
+]);
